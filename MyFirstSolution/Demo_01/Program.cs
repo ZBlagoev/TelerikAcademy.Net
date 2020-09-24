809e1dc09217bc62dfa638d3e1f887fe740283e6
BZB <zhelyazkoblagoev@gmail.com>
2020-09-24 14:46:57 +0300

    initial commit of demo one code

---
+++ MyFirstSolution/Demo_01/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Data;
+
+namespace Demo_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Pesho";
+
+            const string LastName = "Peshev";
+
+            Console.WriteLine(firstName,LastName);
+
+        }
+    }
+}
