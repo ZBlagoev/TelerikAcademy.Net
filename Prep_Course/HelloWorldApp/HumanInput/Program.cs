d6247301b1de6bfc4fa51be36f9f7a94b49eba58
Zhelyazko Blagoev <zhelyazko.blagoev@outlook.com>
2020-10-11 11:16:37 +0300

    add solution to greet you by name

---
+++ Prep_Course/HelloWorldApp/HumanInput/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HumanInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // input
+            string name = Console.ReadLine();
+
+            // output
+            Console.WriteLine("Hello {0}", name);
+        }
+    }
+}
