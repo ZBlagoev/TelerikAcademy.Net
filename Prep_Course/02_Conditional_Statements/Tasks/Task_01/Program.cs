45ef5df249204babee420211aacc4bae0035e751
Zhelyazko Blagoev <zhelyazko.blagoev@outlook.com>
2020-10-19 13:24:40 +0300

    Tasks conditional statements - submit Task 01 and task 02

---
+++ Prep_Course/02_Conditional_Statements/Tasks/Task_01/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+Most people believe that 1 human year (~HY~) equals 7 dog years (~DY~), however, dogs reach adulthood in approximately 2 human years.
+
+So it's better to count the first two HY as 10 DY each and then count the remaining HY as 4 DY each.
+
+Write a program that correctly converts human years (HY) to dog years (DY). You may need some form of conditional logic.
+*/
+
+using System;
+
+namespace Task_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // input
+            int humanYears = int.Parse(Console.ReadLine());
+            int dogYears;
+
+            // calculation
+            if (humanYears <= 2)
+            {
+                dogYears = 10 * humanYears;
+            }
+            else
+            {
+                dogYears = 4 * (humanYears - 2) + 20;
+            }
+
+            // output
+            Console.WriteLine(dogYears);
+        }
+    }
+}
---
+++ Prep_Course/02_Conditional_Statements/Tasks/Task_02/Program.cs
@@ -0,0 +1,39 @@
+﻿/*
+Write a program that determines the color of a chessboard square based on its Label and Rank
+
+Labels have values from a to h
+Ranks have values from 1 to 8
+*/
+
+using System;
+
+namespace Task_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // input
+            char label = char.Parse(Console.ReadLine());
+            int rank = int.Parse(Console.ReadLine());
+            int labelNum = (int)label - 32;
+            string boardColor = "dark";
+
+            // calculation
+            if (rank % 2 == 0)
+            {
+                if (labelNum % 2 != 0)
+                {
+                    boardColor = "light";
+                }
+            }
+            else if (labelNum % 2 == 0)
+            {
+                boardColor = "light";
+            }
+
+            // output 
+            Console.WriteLine(boardColor);
+        }
+    }
+}
