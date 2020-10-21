3aba8aaf6f13a22ada9347f47f996e6a5aac71c0
ZBlagoev <52703098+ZBlagoev@users.noreply.github.com>
2020-10-21 16:39:41 +0300

    HW conditional statements - HW_05 submit

---
+++ Prep_Course/02_Conditional_Statements/HomeWork/Problem 5. The Biggest of 3 Numbers/Program.cs
@@ -0,0 +1,47 @@
+﻿/*
+Problem 5. The Biggest of 3 Numbers
+Write a program that finds the biggest of three numbers.
+*/
+
+using System;
+
+namespace Problem_5._The_Biggest_of_3_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // input
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double c = double.Parse(Console.ReadLine());
+
+            double biggest;
+
+            // calculation
+            if (a > b)
+            {
+                if (a > c)
+                {
+                    biggest = a;
+                }
+                else
+                {
+                    biggest = c;
+                }
+            }
+            else if (b > c)
+            {
+                biggest = b;
+            }
+            else
+            {
+                biggest = c;
+            }
+
+            // output
+            Console.WriteLine(biggest);
+
+        }
+    }
+}
