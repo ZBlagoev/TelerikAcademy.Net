36d44913b6e2dcec39cff760dc46e0edf1286d53
ZBlagoev <52703098+ZBlagoev@users.noreply.github.com>
2020-10-21 17:19:15 +0300

    HW conditional statements - HW_10 added

---
+++ Prep_Course/02_Conditional_Statements/HomeWork/Problem 10. Beer Time/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Problem_10._Beer_Time
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
