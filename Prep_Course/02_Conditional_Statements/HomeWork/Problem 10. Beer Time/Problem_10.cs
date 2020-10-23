0393a4d9325a2a1b2efe653c029f93047be38941
ZBlagoev <52703098+ZBlagoev@users.noreply.github.com>
2020-10-23 11:09:01 +0300

    HW conditional statements - HW_10 comment added

--- Prep_Course/02_Conditional_Statements/HomeWork/Problem 10. Beer Time/Problem_10.cs
+++ Prep_Course/02_Conditional_Statements/HomeWork/Problem 10. Beer Time/Problem_10.cs
@@ -45,7 +45,7 @@ namespace Problem_10._Beer_Time
 
             // calculation
             if (DateTime.TryParseExact(time, format, System.Globalization.CultureInfo.InvariantCulture,
-                DateTimeStyles.None, out dDate)) // second part of that if ensures input is in AM/PM form
+                DateTimeStyles.None, out dDate)) // ensures input is in AM/PM form only (revert for previous commit for every datetime format)
             {
                 if ((Convert.ToDateTime(time) >= Convert.ToDateTime("1:00 PM")) || (Convert.ToDateTime(time) <= Convert.ToDateTime("3:00 AM")))
                 {
