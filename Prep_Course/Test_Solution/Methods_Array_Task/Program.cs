8b9013b9c01c17051de939c098aef8007401b9dc
ZBlagoev <52703098+ZBlagoev@users.noreply.github.com>
2020-10-26 11:18:28 +0200

    test - added array task method

---
+++ Prep_Course/Test_Solution/Methods_Array_Task/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace Methods_Array_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new int[] { 1, 2, 3 };
+            Increment(arr);
+
+            Console.WriteLine(string.Join(" ", arr));
+        }
+
+        static void Increment(int [] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i]++;
+            }
+        }
+
+    }
+}
