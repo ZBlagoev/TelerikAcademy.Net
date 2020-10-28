76feba013cb012101e44841f9c2fa837a0e181fa
ZBlagoev <52703098+ZBlagoev@users.noreply.github.com>
2020-10-28 20:00:30 +0200

    HW Loops - HW_01 submti

---
+++ Prep_Course/03_Loops/HomeWork/Problem 1. Numbers from 1 to N/Problem_01.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Problem_1._Numbers_from_1_to_N
+{
+    class Problem_01
+    {
+        static void Main(string[] args)
+        {
+            // input
+            Console.WriteLine("Problem 1. Numbers from 1 to N");
+
+            Console.Write("n: ");
+            int n = int.Parse(Console.ReadLine());
+            string result = "";
+
+            // calculation
+            for (int i = 1; i <= n; i++)
+            {
+                result += $"{i} ";
+            }
+
+            // output
+            Console.WriteLine($"result: {result}");
+
+        }
+    }
+}
