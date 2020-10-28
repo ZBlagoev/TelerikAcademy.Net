5f26adf6157ca1365ed5c331b345d0fc139e0aee
ZBlagoev <52703098+ZBlagoev@users.noreply.github.com>
2020-10-28 15:22:37 +0200

    tasks Loops - Task_13 submit

---
+++ Prep_Course/03_Loops/Tasks/Task_13/Task_13.cs
@@ -0,0 +1,51 @@
+﻿/*
+Prime Factors
+Write a program that finds the prime factors of a given non-prime number. 
+List the factors in ascending order.
+
+12 = 2 2 3
+100 = 2 2 5 * 5
+Input
+On the first line you will receive one number - N
+Output
+Output each prime factor on a newline.
+Constraints
+4 <= N <= 1000
+N is guaranteed to not be a prime
+*/
+
+using System;
+
+namespace Task_13
+{
+    class Task_13
+    {
+        static void Main(string[] args)
+        {
+            // input
+            int a = int.Parse(Console.ReadLine());
+
+            // calculation and output
+            for (int b = 2; a > 1; b++)
+            {
+                if (a % b == 0)
+                {
+                    int x = 0;
+                    while (a % b == 0)
+                    {
+                        a /= b;
+                        x++;
+                    }
+
+                    for (int i = x; i >= 1 ; i--)
+                    {
+                        Console.WriteLine(b);
+                    }
+                    
+                }
+            }
+
+                
+        }
+    }
+}
