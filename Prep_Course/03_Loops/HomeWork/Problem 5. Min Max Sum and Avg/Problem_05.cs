b0a405bd01e9e223172852db6939e30d8d2fece9
ZBlagoev <52703098+ZBlagoev@users.noreply.github.com>
2020-10-28 21:56:31 +0200

    HW Loops - HW_06 added + HW_05 description added

--- Prep_Course/03_Loops/HomeWork/Problem 5. Min Max Sum and Avg/Problem_05.cs
+++ Prep_Course/03_Loops/HomeWork/Problem 5. Min Max Sum and Avg/Problem_05.cs
@@ -1,4 +1,14 @@
-﻿using System;
+﻿/*
+Write a program that reads from the console a sequence of n integer numbers 
+and returns the minimal, the maximal number, the sum and the average of 
+all numbers (displayed with 2 digits after the decimal point).
+
+The input starts by the number n (alone in a line) followed by n lines, 
+each holding an integer number.
+The output is like in the examples below.
+*/
+
+using System;
 
 namespace Problem_5._Min_Max_Sum_and_Avg
 {
---
+++ Prep_Course/03_Loops/HomeWork/Problem 6. Word or Number/Problem_06.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Problem_6._Word_or_Number
+{
+    class Problem_06
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
