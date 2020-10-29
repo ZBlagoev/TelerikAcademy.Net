72a5e05e33009113594cc398827a943679853c7d
ZBlagoev <52703098+ZBlagoev@users.noreply.github.com>
2020-10-29 12:47:49 +0200

    HW Loops - HW_93 submit + deelete test

--- Prep_Course/03_Loops/HomeWork/Problem 99. Odd and Even Product/Problem_93.cs
+++ Prep_Course/03_Loops/HomeWork/Problem 99. Odd and Even Product/Problem_93.cs
@@ -1,4 +1,14 @@
-﻿using System;
+﻿/*
+You are given n integers (given in a single line, separated by a space).
+
+Write a program that checks whether the product of the odd elements is 
+equal to the product of the even elements.
+
+Elements are counted from 1 to n, so the first element is odd, the second is even, etc.
+*/
+
+using System;
+using System.Collections.Generic;
 
 namespace Problem_99._Odd_and_Even_Product
 {
@@ -6,7 +16,69 @@ namespace Problem_99._Odd_and_Even_Product
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // input
+            Console.WriteLine("Additional NON-Required Problem: Odd and Even Product");
+
+            Console.Write("input: ");
+            string input = Console.ReadLine();
+
+            string[] numStr = input.Split(" ");
+            int n = numStr.Length;
+            int[] numbers = Array.ConvertAll(numStr, int.Parse);
+
+            // placeholders for the number of odd and even int below 'n' 
+            int even = (n - 1 + (1 % 2)) / 2 + (1 - (1 % 2));
+            int odd = n - even;          
+            
+            List<int> evenN = new List<int>();
+            List<int> oddN = new List<int>();
+
+            int prodEven = 1;
+            int prodOdd = 1;
+
+            // calculation
+                // populate even and odd lists
+                // note: values of odd and even must be flipped because index starts at 0
+            for (int i = 0; i < n; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    oddN.Add(numbers[i]);
+                }
+                else
+                {
+                    evenN.Add(numbers[i]);
+                }
+            }
+
+                // calculate products
+            foreach (int number in evenN)
+            {
+                prodEven *= number;
+            }
+
+            foreach (int number in oddN)
+            {
+                prodOdd *= number;
+            }
+
+            // output
+            Console.WriteLine("result: ");
+
+            if (prodEven == prodOdd)
+            {
+                Console.WriteLine("yes");
+                Console.WriteLine($"product = {prodOdd}");
+            }
+            else
+            {
+                Console.WriteLine("no");
+                Console.WriteLine($"odd_product = {prodOdd}");
+                Console.WriteLine($"even_product = {prodEven}");
+            }
+
+
+
         }
     }
 }
---
+++ Prep_Course/03_Loops/HomeWork/test/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // input
+            int n = int.Parse(Console.ReadLine());
+
+            int oddN;
+            int evenN;
+
+            //calculation
+            evenN = (n - 1 + (1 % 2)) / 2 + (1 - (1 % 2));
+            oddN = n - evenN;
+
+            // output
+            Console.WriteLine(n);
+            Console.WriteLine(evenN);
+            Console.WriteLine(oddN);
+        }
+    }
+}
