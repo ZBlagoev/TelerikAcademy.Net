2749cdac603f22a792aebabc4b7f8cf76f4071f9
ZBlagoev <52703098+ZBlagoev@users.noreply.github.com>
2020-10-29 13:06:13 +0200

    HW Loops - HW_92 submit

--- Prep_Course/03_Loops/HomeWork/Problem 99. Calculate N K/Problem_92.cs
+++ Prep_Course/03_Loops/HomeWork/Problem 99. Calculate N K/Problem_92.cs
@@ -13,7 +13,12 @@ namespace Problem_99._Calculate_N_K
         static void Main(string[] args)
         {
             // input
+            Console.WriteLine("Additional NON-Required Problem: Calculate N! / K!");
+
+            Console.Write("n: ");
             int n = int.Parse(Console.ReadLine());
+            Console.Write("k: ");
+            int k = int.Parse(Console.ReadLine());
 
             int factorial = 1;
 
@@ -22,10 +27,15 @@ namespace Problem_99._Calculate_N_K
             {
                 factorial *= i;
 
+                if (i <= k)
+                {
+                    factorial /= i;
+                }
+
             }
 
             // output
-            Console.WriteLine(factorial);
+            Console.WriteLine($"n!/k!: {factorial}");
 
         }
     }
