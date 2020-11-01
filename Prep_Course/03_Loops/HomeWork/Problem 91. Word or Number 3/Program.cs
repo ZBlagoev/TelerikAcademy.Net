f3296c036a36b664b170df8af48a43fb40fe582c
ZBlagoev <zhelyazkoblagoev@gmail.com>
2020-11-01 18:27:49 +0200

    HW Loops - HW_91 added

---
+++ Prep_Course/03_Loops/HomeWork/Problem 91. Word or Number 3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Problem_91._Word_or_Number_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
