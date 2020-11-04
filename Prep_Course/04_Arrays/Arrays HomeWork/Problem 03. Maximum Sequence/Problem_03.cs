73beaef134f3229be874c6b3bec8adad927496bc
ZBlagoev <zhelyazkoblagoev@gmail.com>
2020-11-04 19:23:34 +0200

    HW Arrays - HW_03 submit

---
+++ Prep_Course/04_Arrays/Arrays HomeWork/Problem 03. Maximum Sequence/Problem_03.cs
@@ -0,0 +1,54 @@
+﻿/*
+Write a program that finds the length of the maximum sequence 
+of equal elements in an array of integers.
+*/
+
+using System;
+using System.Linq;
+
+namespace Problem_03._Maximum_Sequence
+{
+    class Problem_03
+    {
+        static void Main(string[] args)
+        {
+            // input
+            Console.WriteLine("Problem 3. Maximum Sequence");
+
+            Console.Write("input: ");
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int counter = 1;
+            int max = 1;
+
+            int currint = 0;
+
+            // calculation
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (i == 0)
+                {
+                    continue;
+
+                }
+                else if(arr[i - 1] == arr[i])
+                {
+                    counter++;
+                }
+                else
+                {
+                    if (max < counter)
+                    {
+                        max = counter;
+                    }
+
+                    counter = 1;
+                }
+            }
+
+            // output
+            Console.WriteLine($"output: {max}");
+
+        }
+    }
+}
