45f122fff587debd9f8216406c65d7ade9c23786
ZBlagoev <zhelyazkoblagoev@gmail.com>
2020-11-04 19:32:33 +0200

    HW Arrays - HW_04 submit

---
+++ Prep_Course/04_Arrays/Arrays HomeWork/Problem 04. Is Array Sorted/Problem_04.cs
@@ -0,0 +1,43 @@
+﻿/*
+Write a program that determines if an array of 
+integers is sorted in increasing order.
+*/
+
+using System;
+using System.Linq;
+
+namespace Problem_04._Is_Array_Sorted
+{
+    class Problem_04
+    {
+        static void Main(string[] args)
+        {
+            // input
+            Console.WriteLine("Problem 4. Is Array Sorted?");
+
+            Console.Write("input: ");
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            string result = "Yes";
+
+            // calculation
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i - 1] < arr[i])
+                {
+                    continue;
+                }
+                else
+                {
+                    result = "No";
+                    break;
+                }
+            }
+
+            // output
+            Console.WriteLine($"output: {result}");
+
+
+        }
+    }
+}
