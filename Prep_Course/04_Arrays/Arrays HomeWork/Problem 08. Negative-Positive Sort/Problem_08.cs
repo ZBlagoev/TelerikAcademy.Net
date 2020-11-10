62dd72c2249bcee5f668ade9f8963125b580aabc
ZBlagoev <zhelyazkoblagoev@gmail.com>
2020-11-10 12:34:24 +0200

    HW Arrays - HW_08 submit + HW_11 added

--- Prep_Course/04_Arrays/Arrays HomeWork/Problem 08. Negative-Positive Sort/Problem_08.cs
+++ Prep_Course/04_Arrays/Arrays HomeWork/Problem 08. Negative-Positive Sort/Problem_08.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/*
+Write a program that moves all negative numbers to the beginning 
+and all positive to the end, without changing the order of positive negative
+*/
+
+using System;
+using System.Linq;
 
 namespace Problem_08._Negative_Positive_Sort
 {
@@ -6,7 +12,31 @@ namespace Problem_08._Negative_Positive_Sort
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // input 
+            Console.WriteLine("Problem 8. Negative-Positive Sort");
+
+            Console.Write("input: ");
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            string positive = "";
+            string negative = "";
+
+            // calculation
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] >= 0)
+                {
+                    positive += " " + arr[i].ToString();
+                }
+                else
+                {
+                    negative += " " + arr[i].ToString();
+                }
+            }
+
+            // output
+            Console.Write($"output:{negative}{positive}");
+
         }
     }
 }
---
+++ Prep_Course/04_Arrays/Arrays HomeWork/Problem 11. Pairs/Problem_11.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Problem_11._Pairs
+{
+    class Problem_11
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
