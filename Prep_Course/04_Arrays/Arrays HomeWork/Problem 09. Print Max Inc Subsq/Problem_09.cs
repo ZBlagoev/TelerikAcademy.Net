170531af33f0d27a188d1cb5560469289271213c
ZBlagoev <zhelyazkoblagoev@gmail.com>
2020-11-10 15:17:27 +0200

    HW Arrays - HW_09 solution started

--- Prep_Course/04_Arrays/Arrays HomeWork/Problem 09. Print Max Inc Subsq/Problem_09.cs
+++ Prep_Course/04_Arrays/Arrays HomeWork/Problem 09. Print Max Inc Subsq/Problem_09.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/*
+Write a program that prints the maximum length increasing subsequence to the console. 
+If two or more are of the same length, print the last one
+*/
+
+using System;
+using System.Linq;
 
 namespace Problem_09._Print_Max_Inc_Subsq
 {
@@ -6,7 +12,26 @@ namespace Problem_09._Print_Max_Inc_Subsq
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // input 
+            Console.WriteLine("Problem 9. Print Maximum Increasing Subsequence");
+
+            Console.Write("input: ");
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            string sequence = "";
+
+            // calculation
+            for (int i = 1; i < arr.Length; i++)
+            {
+
+            }
+
+
+
+
+
+
+
         }
     }
 }
