0766324be7f92026ec7fea0d0e688cdb341a7735
ZBlagoev <zhelyazkoblagoev@gmail.com>
2020-11-12 09:03:42 +0200

    Additional_tasks - added test folder + HW Arrays - removed test folder

---
+++ Prep_Course/05_Additional_tasks/Part_one/test/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = int.Parse(Console.ReadLine());
+
+            int[] numbers = Console.ReadLine().Trim().Split()
+                .Select(x => int.Parse(x)).ToArray();
+
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    if (numbers[i] + numbers[j] == sum)
+                    {
+                        Console.WriteLine($"{numbers[i]}, {numbers[j]}");
+                    }
+                }
+            }
+        }
+    }
+}
