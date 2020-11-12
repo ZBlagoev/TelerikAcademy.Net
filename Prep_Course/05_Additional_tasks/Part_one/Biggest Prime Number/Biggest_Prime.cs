c67a9cb52275b50ef976f16b3c1812cd023f775e
ZBlagoev <zhelyazkoblagoev@gmail.com>
2020-11-12 10:00:41 +0200

    Additional_tasks - Biggest Prime Number submit + Kitty small change

---
+++ Prep_Course/05_Additional_tasks/Part_one/Biggest Prime Number/Biggest_Prime.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Biggest_Prime_Number
+{
+    class Biggest_Prime
+    {
+        static void Main(string[] args)
+        {
+            // input
+            bool[] isPrime = new bool[int.Parse(Console.ReadLine()) + 1];
+
+            for (int i = 2; i < isPrime.Length; i++)
+            {
+                isPrime[i] = true;
+            }
+
+            int prime = 0;
+
+            // calculation
+            for (int j = 2; j < isPrime.Length; j++)
+            {
+                if (isPrime[j])
+                {
+                    for (int p = 2; (p * j) < isPrime.Length; p++)
+                    {
+                        isPrime[p * j] = false;
+                    }
+                }
+            }
+
+            for (int k = 2; k < isPrime.Length; k++)
+            {
+                if (isPrime[k])
+                {
+                    prime = k;
+                }
+            }
+
+            // output
+            Console.WriteLine(prime);
+
+        }
+    }
+}
--- Prep_Course/05_Additional_tasks/Part_one/Kitty/Kitty.cs
+++ Prep_Course/05_Additional_tasks/Part_one/Kitty/Kitty.cs
@@ -1,4 +1,41 @@
-﻿
+﻿/*
+The Kitty is a very dangerous and strange creature. She loves to eat but also loves 
+to collect souls. Not ordinary souls but the souls of coders. In order to not collect 
+your soul Kitty assigned you a task to find how much food and souls she could collect 
+in any iteration. Kitty doesn't know she can be deadlocked and the rest of the coders 
+will be saved. There is a catch. Kitty could escape a deadlock if she leaves a soul or 
+food in that cell.
+
+Your task is to calculate the food and the souls collected by Kitty or to output that 
+she is deadlocked.
+
+On the first line of the input you will receive the positions of the coder souls ("@"),
+food ("*") and deadlocks ("x") as string.
+
+On the second line of the input you will receive the path of the Kitty as string with 
+integers separated by single space. Positive means, move to the right, negative means, 
+move to the left.
+
+The starting position will always be at index 0.
+
+The final result is either the souls, food and deadlocks count or a string informing 
+that the Kitty is deadlocked. The format is shown in the zero tests and the example.
+
+@ - symbol for coder soul
+* - symbol for food
+x - symbol for deadlock
+
+We start at position 0 and collect a soul @
+We move 1 time to the right and now we are at position 1 and collect a soul @
+We move 1 time to the left and now we are at position 0 and nothing is there because we 
+already collected it, so we continue our path.
+We move 1 time to the left and now we are at position 6 (if you exit the array you should 
+enter from the other side) and we collect food *
+We move 4 times to the right and now we are at position 3 and there is a deadlock.
+If the deadlock is on even position the Kitty should leave a soul (@) there in order to escape.
+If the deadlock is on odd position the Kitty should leave food (*) there in order to escape.
+Position 3 is odd so we leave food (*) to escape the deadlock.
+*/
 
 using System;
 using System.Linq;
