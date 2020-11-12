dad89be47c83e895d48519b063597609db8e40d2
ZBlagoev <zhelyazkoblagoev@gmail.com>
2020-11-12 10:17:07 +0200

    Additional_tasks - Long seq submit + max sum of k submit

---
+++ Prep_Course/05_Additional_tasks/Part_one/Long seq of equal/long_seq_eq.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Long_seq_of_equal
+{
+    class long_seq_eq
+    {
+        static void Main(string[] args)
+        {
+            // input
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            int counter = 1;
+            int max = 1;
+
+            // calculation
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (i == 0)
+                {
+                    continue;
+
+                }
+                else if (arr[i - 1] == arr[i])
+                {
+                    counter++;
+                }
+                else
+                {
+                    if (max < counter)
+                    {
+                        max = counter;
+                    }
+
+                    counter = 1;
+                }
+            }
+
+            // output
+            Console.WriteLine(max);
+        }
+    }
+}
---
+++ Prep_Course/05_Additional_tasks/Part_one/Max Sum of K/Max_sum_k.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Max_Sum_of_K
+{
+    class Max_sum_k
+    {
+        static void Main(string[] args)
+        {
+            // input
+            int n = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+
+            int[] nArr = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                nArr[i] = int.Parse(Console.ReadLine());
+            }
+
+            int sum = 0;
+
+            Array.Sort(nArr);
+
+            // caluclation
+            for (int j = n - 1; j > n - 1 -k; j--)
+            {
+                sum += nArr[j];
+            }
+
+            // output
+            Console.WriteLine(sum);
+
+
+        }
+    }
+}
