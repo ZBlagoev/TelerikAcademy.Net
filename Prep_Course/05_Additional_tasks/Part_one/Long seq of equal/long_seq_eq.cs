46e4d16868dfbadf3ba91c5f5b07c61d65eb784e
ZBlagoev <zhelyazkoblagoev@gmail.com>
2020-11-12 11:49:34 +0200

    additional tasks - added descriptions

--- Prep_Course/05_Additional_tasks/Part_one/Long seq of equal/long_seq_eq.cs
+++ Prep_Course/05_Additional_tasks/Part_one/Long seq of equal/long_seq_eq.cs
@@ -1,4 +1,16 @@
-﻿using System;
+﻿/*
+Description
+Write a program that finds the length of the maximal sequence 
+of equal elements in an array of N integers.
+
+Input
+On the first line you will receive the number N
+On the next N lines the numbers of the array will be given
+Output
+Print the length of the maximal sequence
+*/
+
+using System;
 
 namespace Long_seq_of_equal
 {
--- Prep_Course/05_Additional_tasks/Part_one/Max Sum of K/Max_sum_k.cs
+++ Prep_Course/05_Additional_tasks/Part_one/Max Sum of K/Max_sum_k.cs
@@ -1,4 +1,17 @@
-﻿using System;
+﻿/*
+Write a program that reads two integer numbers N and K and 
+an array of N elements from the console. Find the maximal sum 
+of K elements in the array.
+
+Input
+On the first line you will receive the number N
+On the second line you will receive the number K
+On the next N lines the numbers of the array will be given
+Output
+Print the maximal sum of K elements in the array
+*/
+
+using System;
 
 namespace Max_Sum_of_K
 {
