f52b20e3b4a731fb1d1b148e3cd03b3ec4347271
ZBlagoev <zhelyazkoblagoev@gmail.com>
2020-12-11 20:45:36 +0200

    Module_00 - Data_types_advanced - HW_3 submit

---
+++ Module_00/01_Data_Types_Advanced_tasks/Mirror_codes/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace Mirror_codes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Console.WriteLine(Pincode(input));
+        }
+
+        public static int Pincode(Array input)
+        {
+            int result = 0;
+
+            foreach (int item in input)
+            {
+                int reverse = 0;
+                int number = item;
+
+                while (number > 0)
+                {
+                    int remainder = number % 10;
+                    reverse = (reverse * 10) + remainder;
+                    number /= 10;
+                }
+
+                if (reverse > result)
+                {
+                    result = reverse;
+                }
+            }
+
+            return result;
+        }
+    }
+}
