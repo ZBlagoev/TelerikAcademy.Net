9f7f1ba0a792d91fd1c4c5eff14ac74a34afae0d
ZBlagoev <zhelyazkoblagoev@gmail.com>
2020-12-11 21:09:44 +0200

    Module_00 - Data_types_advanced - HW_4 submit

---
+++ Module_00/01_Data_Types_Advanced_tasks/Ordering/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+namespace Ordering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Console.WriteLine(SortPots(input));
+        }
+
+        static string SortPots(int[] input)
+        {
+            int[] ascending = new int[input.Length];
+            Array.Copy(input, ascending, input.Length);
+
+            int[] descending = new int[input.Length];
+            Array.Copy(input, descending, input.Length);
+
+            Array.Sort(ascending);
+            Array.Sort(descending);
+
+            Array.Reverse(descending);
+
+            string result = "";
+
+            if (ascending.SequenceEqual(input))
+            {
+                result = "Ascending";
+            }
+            else if (descending.SequenceEqual(input))
+            {
+                result = "Descending";
+            }
+            else
+            {
+                result = "Mixed";
+            }
+
+            return result;
+        }
+    }
+}
