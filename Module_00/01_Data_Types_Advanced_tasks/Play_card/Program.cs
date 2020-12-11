213785e2ce0eb6359487cb7fb0cc5ece896cc084
ZBlagoev <zhelyazkoblagoev@gmail.com>
2020-12-11 21:20:33 +0200

    Module_00 - Data_types_advanced - HW_5 submit

---
+++ Module_00/01_Data_Types_Advanced_tasks/Play_card/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace Play_card
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Console.WriteLine(PlayCardCheck(input));
+        }
+
+        static string PlayCardCheck(string input)
+        {
+            string[] cards = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A" };
+
+            string result = "";
+
+            if (cards.Contains(input))
+            {
+                result = $"yes {input}";
+            }
+            else
+            {
+                result = $"no {input}";
+            }
+
+            return result;
+        }
+
+    }
+}
