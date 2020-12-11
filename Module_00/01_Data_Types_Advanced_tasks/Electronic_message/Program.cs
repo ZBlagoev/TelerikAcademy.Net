94f7bcc60201807fff84668a950ce789f3574995
ZBlagoev <zhelyazkoblagoev@gmail.com>
2020-12-11 17:20:30 +0200

    Module_00 - Data_types_advanced - HW_1 + HW_2 submit

---
+++ Module_00/01_Data_Types_Advanced_tasks/Electronic_message/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Electronic_message
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Console.WriteLine(ElMessageChecker(input));
+        }
+
+
+        public static int ElMessageChecker(string input)
+        {
+            input = input.Remove(input.Length - 1);
+
+            int result = 0;
+            int length = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (!(
+                    (input[i] >= 48 && input[i] < 57)
+                    || (input[i] >= 65 && input[i] < 90)
+                    || (input[i] >= 97 && input[i] < 122)
+                    || (input[i] == 32)
+                    ))
+                {
+                    length++;
+
+                    if (length > result)
+                    {
+                        result = length;
+                    }
+                }
+                else
+                {
+                    length = 0;
+                }
+
+            }
+
+            return result;
+        }
+
+
+    }
+}
---
+++ Module_00/01_Data_Types_Advanced_tasks/Game/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // input
+            int input = int.Parse(Console.ReadLine());
+
+            // calculation
+            int result = Math.Max((input / 100) * (input / 10 % 10) * (input % 10),
+                Math.Max((input / 100) * (input / 10 % 10) + (input % 10),
+                Math.Max((input / 100) + (input / 10 % 10) * (input % 10),
+                (input / 100) + (input / 10 % 10) + (input % 10))));
+
+            // output
+            Console.WriteLine(result);
+        }
+    }
+}
