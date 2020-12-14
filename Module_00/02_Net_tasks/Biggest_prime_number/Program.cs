07d76e5725f2bc59f85cb21f246249dfe218503d
ZBlagoev <zhelyazkoblagoev@gmail.com>
2020-12-14 10:21:53 +0200

    Module_0 - Net_tasks_02 - Task_01 submit

---
+++ Module_00/02_Net_tasks/Biggest_prime_number/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Biggest_prime_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(BiggestPrime(number));
+        }
+
+        static int BiggestPrime(int numberUP)
+        {
+            bool[] isPrime = new bool[numberUP + 1];
+
+            for (int i = 2; i < isPrime.Length; i++)
+            {
+                isPrime[i] = true;
+            }
+
+            int result = 0;
+
+            for (int j = 2; j < isPrime.Length; j++)
+            {
+                if (isPrime[j])
+                {
+                    for (int k = 2; (k * j) < isPrime.Length; k++)
+                    {
+                        isPrime[k * j] = false;
+                    }
+                }
+            }
+
+            for (int l = isPrime.Length - 1; l >= 2; l--)
+            {
+                result = l;
+
+                if (isPrime[l] == true)
+                {
+                    break;
+                }
+            }
+
+            return result;
+        }
+    }
+}
