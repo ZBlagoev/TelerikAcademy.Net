8b3d76873b51f3f8f27a1561954f75071524e645
ZBlagoev <zhelyazkoblagoev@gmail.com>
2020-12-14 10:46:07 +0200

    Module_0 - Net_Tasks_02 - Task_02 submit

---
+++ Module_00/02_Net_tasks/Forrest_road/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Forrest_road
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            ForrestPath(number);
+        }
+
+        static void ForrestPath(int number)
+        {
+            TopPart(number);
+            BottomPart(number);
+        }
+
+        static void TopPart (int number)
+        {
+            int current = 1;
+
+            for (int i = 1; i <= number; i++)
+            {
+                for (int j = 1; j <= number; j++)
+                {
+                    if (j == current)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(".");
+                    }
+                }
+
+                Console.WriteLine();
+                current++;
+            }
+        }
+
+        static void BottomPart (int number)
+        {
+            int current = number - 1;
+
+            for (int i = 1; i < number; i++)
+            {
+                for (int j = 1; j <= number; j++)
+                {
+                    if (j == current)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(".");
+                    }
+                }
+
+                Console.WriteLine();
+                current--;
+            }
+        }
+    }
+}
