c6d52f752aa2f680ae4c0f457aff8ceba4002546
ZBlagoev <zhelyazkoblagoev@gmail.com>
2020-12-15 14:18:17 +0200

    module_0 - net_tasks_2 - added description to task_1, task_2, task_5

--- Module_00/02_Net_tasks/Biggest_prime_number/Program.cs
+++ Module_00/02_Net_tasks/Biggest_prime_number/Program.cs
@@ -1,4 +1,18 @@
-﻿using System;
+﻿/*
+Biggest Prime Number
+Description
+Write a program that finds all prime numbers in the range 1 ... N. Use the Sieve of Eratosthenes algorithm. 
+The program should print the biggest prime number which is <= N.
+
+Input
+On the first line you will receive the number N
+Output
+Print the biggest prime number which is <= N
+Constraints
+2 <= N <= 10 000 000
+*/
+
+using System;
 
 namespace Biggest_prime_number
 {
