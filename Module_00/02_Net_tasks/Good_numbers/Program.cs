0e432464f9843c4874a41f3ff2519fe03901d507
ZBlagoev <zhelyazkoblagoev@gmail.com>
2020-12-15 11:34:36 +0200

    Module_00 - net_tasks_02 - task_05 - submit (also build logic for testing the input)

---
+++ Module_00/02_Net_tasks/Good_numbers/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Linq;
+
+namespace Good_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+
+            bool error = false;
+
+            int[] borders = new int[2];
+
+            do
+            {
+                input = Console.ReadLine();
+
+                try
+                {
+                    borders = input.Split().Select(int.Parse).ToArray();
+                    error = false;
+                }
+                catch (FormatException e)
+                {
+                    error = true;
+                    Console.WriteLine("Please input two int numbers separated by space", e);
+                }
+
+            } while (error);
+
+            Console.WriteLine(GoodNumbersFinder(borders));
+        }
+
+
+        static int GoodNumbersFinder(int[] borders)
+        {            
+            int goodNumbers = 0;
+
+            for (int number = borders[0]; number <= borders[1]; number++)
+            {
+                int buffer = number;
+
+                int alarm = 0;
+
+                while (buffer != 0)
+                {
+                    int current = buffer % 10;
+
+                    if (current == 0)
+                    {
+                        buffer /= 10;
+                        continue;
+                    }
+
+                    if (number % current != 0)
+                    {
+                        alarm++;
+                        break;
+                    }
+
+                    buffer /= 10;
+                }
+
+                if (alarm == 0)
+                {
+                    goodNumbers++;
+                }
+
+            }
+
+            return goodNumbers;
+        }
+
+
+
+    }
+}
