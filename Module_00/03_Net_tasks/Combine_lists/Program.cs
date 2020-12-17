dfb635fb302e1eb7de81227a2346ab6e89d0b1ea
ZBlagoev <zhelyazkoblagoev@gmail.com>
2020-12-17 13:25:48 +0200

    module_0 - net_tasks_3 - task_3 - submit

---
+++ Module_00/03_Net_tasks/Combine_lists/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Combine_lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> first = Console.ReadLine().Split(',').Select(int.Parse).ToList();
+            List<int> second = Console.ReadLine().Split(',').Select(int.Parse).ToList();
+
+            Console.WriteLine(String.Join(",",CombineLists(first, second)));            
+        }
+
+
+        static List<int> CombineLists(List<int> first, List<int> second)
+        {
+            List<int> combined = new List<int>();
+
+            for (int i = 0; i < first.Count; i++)
+            {
+                combined.Add(first[i]);
+                combined.Add(second[i]);
+            }
+
+            return combined;
+        }
+    }
+}
