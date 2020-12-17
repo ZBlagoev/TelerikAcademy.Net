be354709517a89c0768f66b2931d1cf31d7b243a
ZBlagoev <zhelyazkoblagoev@gmail.com>
2020-12-17 15:39:26 +0200

    module_0 - net_tasks_3 - task_1 - submit

---
+++ Module_00/03_Net_tasks/Array_search/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Array_search
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MissingElement(Console.ReadLine().Split(',').Select(int.Parse).ToArray());
+        }
+
+        static void MissingElement(int[] input)
+        {
+            List<int> missing = new List<int>();
+
+            for (int i = 1; i <= input.Length; i++)
+            {
+                if (!input.Contains(i))
+                {
+                    missing.Add(i);
+                }
+            }
+
+            Console.WriteLine(String.Join(",",missing));
+        }
+    }
+}
