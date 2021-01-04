e607bcafad65a08a95159bee33a74afa0cdba5c0
ZBlagoev <zhelyazkoblagoev@gmail.com>
2021-01-04 19:18:46 +0200

    added tasks for Multidimentional arrays - 2nd task is not complete - error for last case

---
+++ Module_00/04_Additional_training/test/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix =
+            {
+                { 1, 2
+                },
+                { 3, 4
+                }
+            };
+
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write("{0, 4}", matrix[i, j]);
+                }
+
+                Console.WriteLine();
+            }
+
+
+        }
+
+    }
+}
---
+++ Module_01/01_Multidimentional_arrays_tasks/Symetric Arrays/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Linq;
+
+namespace Symetric_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // input
+            int rows = int.Parse(Console.ReadLine());
+
+
+            // initialize input jagged array
+            string[][] jagged = new string[rows][];
+
+            // fill input jagged array
+            for (int i = 0; i < rows; i++)
+            {
+                string line = Console.ReadLine();
+
+                string[] arrStrings = line.Split();
+
+                jagged[i] = arrStrings;
+            }
+
+
+            // initialize result array
+            string[] result = new string[rows];
+
+            // fill result array
+            for (int i = 0; i < result.Length; i++)
+            {
+                result[i] = "Yes";
+            }
+
+
+            // calculation
+            for (int i = 0; i < jagged.Length; i++)
+            {
+                string[] currentArr = jagged[i];
+
+                for (int j = 0; j < currentArr.Length; j++)
+                {
+
+                    if (!(currentArr[j] == currentArr[currentArr.Length - 1 - j]))
+                    {
+                        result[i] = "No";
+                        break;
+                    }
+                }
+            }
+
+            // result
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine(result[i]);
+            }
+
+
+
+            /*
+
+            Console.Write("input: ");
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            string result = "Yes";
+
+            // calculation
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == arr[arr.Length - 1 - i])
+                {
+                    continue;
+                }
+                else
+                {
+                    result = "No";
+                }
+            }
+
+            // output
+            Console.WriteLine($"output: {result}");
+            */
+
+        }
+    }
+}
