a386b651e24ec5fb917f1d4d03139a32d80f2f3b
ZBlagoev <zhelyazkoblagoev@gmail.com>
2021-01-10 18:02:45 +0200

    navigation submit

--- Module_01/01_Multidimentional_arrays_tasks/Zig_Zag/Program.cs
+++ Module_01/01_Multidimentional_arrays_tasks/Zig_Zag/Program.cs
@@ -1,31 +1,162 @@
 ﻿using System;
 using System.Linq;
+using System.Numerics;
+using System.Collections.Generic;
 
-namespace Zig_Zag
+namespace Navigation
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int[] dims = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            // input
+            int R = int.Parse(Console.ReadLine());
+            int C = int.Parse(Console.ReadLine());
+            _ = int.Parse(Console.ReadLine());
 
-            int[,] matrix = MakeMatrix(dims[0], dims[1]);
+            int[] code = Console.ReadLine().Split().Select(int.Parse).ToArray();
 
-            PrintMatrix(matrix);
+            BigInteger sum = 1;
+
+            int[] startingPosition = {R - 1, 0};
+
+            List<int[]> passedCells = new List<int[]>();
+            passedCells.Add(new int[2] { R - 1, 0 });
+
+            //int[][] passedCells = MakeJagged(R, C);
+            BigInteger[,] matrix = MakeMatrix(R, C);
+
+            int[][] targets = CodeBreaking(code, R, C);
+
+            // calculation
+            for (int i = 0; i < code.Length; i++)
+            {
+                int[] target = (int[])targets[i].Clone();
+
+                var (Y, sumH) = MoveHorisontally(startingPosition, target, passedCells, matrix);
+                startingPosition[1] = Y;
+                var (X, sumY) = MoveVertically(startingPosition, target, passedCells, matrix);
+                startingPosition[0] = X;
+
+                sum += sumH;
+                sum += sumY;
+            }
+
+            // result
+            Console.WriteLine(sum);
+
+            //PrintMatrix(matrix);
         }
 
+        // method for summing cell values
+        static BigInteger Sum(int[] coordinates, List<int[]> passedCells, BigInteger[,] matrix)
+        {
+            BigInteger sum = 0;
+
+            if (!(passedCells.Any(c => c.SequenceEqual(coordinates))))
+            {
+                passedCells.Add((int[])coordinates.Clone());
+
+                sum += matrix[coordinates[0], coordinates[1]];
+            }
+
+            return sum;
+        }
+
+        // method for horisontal move
+        static (int dim, BigInteger sum) MoveHorisontally(int[] start, int[] target, List<int[]> passedCells, BigInteger[,] matrix)
+        {
+            int[] coordinates = (int[])start.Clone();
+
+            int dim;
+            BigInteger sum = 0;
+
+            if ((target[1] - start[1]) > 0)
+            {
+                for (int i = start[1] + 1; i <= target[1]; i++)
+                {
+                    coordinates[1] = i;
+
+                    sum += Sum(coordinates, passedCells, matrix);
+                }
+            }
+            else
+            {
+                for (int i = start[1] - 1; i >= target[1]; i--)
+                {
+                    coordinates[1] = i;
+
+                    sum += Sum(coordinates, passedCells, matrix);
+                }
+            }
+
+            dim = coordinates[1];
+
+            return (dim, sum);
+        }
+
+        // method for vertical move
+        static (int dim, BigInteger sum) MoveVertically(int[] start, int[] target, List<int[]> passedCells, BigInteger[,] matrix)
+        {
+            int[] coordinates = (int[])start.Clone(); 
+
+            int dim;
+            BigInteger sum = 0;
+
+            if ((target[0] - start[0]) > 0)
+            {
+                for (int i = start[0] + 1; i <= target[0]; i++)
+                {
+                    coordinates[0] = i;
+
+                    sum += Sum(coordinates, passedCells, matrix);
+                }
+            }
+            else
+            {
+                for (int i = start[0] - 1; i >= target[0]; i--)
+                {
+                    coordinates[0] = i;
+
+                    sum += Sum(coordinates, passedCells, matrix);
+                }
+            }
+
+            dim = coordinates[0];
+
+            return (dim, sum);
+        }
+
+        // method for code breaking
+        static int[][] CodeBreaking(int[] code, int R, int C)
+        {
+            int[][] input = new int[code.Length][];
+
+            for (int i = 0; i < code.Length; i++)
+            {
+                int coef = Math.Max(R,C);
+                int row = code[i] / coef;
+                int coll = code[i] % coef;
+
+                input[i] = new int[] { row, coll };
+            }
+
+            return input;
+        }
 
         // method for making the matrix
-        static int[,] MakeMatrix(int n, int m)
+        static BigInteger[,] MakeMatrix(int n, int m)
         {
             // making the matrix
-            int[,] matrix = new int[n, m];
+            BigInteger[,] matrix = new BigInteger[n, m];
 
-            for (int i = n - 1; i >= 0; i++)
+            int x = 0; 
+            for (int i = n - 1; i >= 0; i--, x++)
             {
+                
                 for (int j = 0; j < m; j++)
                 {
-                    matrix[i, j] = (int)Math.Pow(2, (i + j));
+                    matrix[i, j] = BigInteger.Pow(2, (x + j));
                 }
             }
 
@@ -33,7 +164,7 @@ namespace Zig_Zag
         }
 
         // method for printing matrix
-        static void PrintMatrix(int[,] input)
+        static void PrintMatrix(BigInteger[,] input)
         {
             for (int i = 0; i < input.GetLength(0); i++)
             {
--- Module_01/01_Multidimentional_arrays_tasks/test/test.cs
+++ Module_01/01_Multidimentional_arrays_tasks/test/test.cs
@@ -1,135 +1,46 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 
 class Program
 {
-
     static void Main(string[] args)
     {
-        string input = Console.ReadLine();
-        string[] array = input.Split();
-        int N = int.Parse(array[0]);
-        int M = int.Parse(array[1]);
-        short[,] arr = ArrayFiller(5, M);
-        //ulong result = ArrayCalculator(arr, N, M); // While doing the task with a whole array is possible, judge memory limit won't give you the maximum points.
-        ulong baseResult = ArrayCalculator(arr, 2, M) - 1;
-        ulong differenceInRows = ArrayCalculator(arr, 4, M) - ArrayCalculator(arr, 3, M) - baseResult;
-        // Console.WriteLine(result);
-        //Console.WriteLine(baseResult);        //at the smallest ammount of rows (2) the base case can be found. You need to remove position[0,0] (-1) to get it correct.
-        //Console.WriteLine(differenceInRows);  //at rows %2 = 0 the difference between rows grows, but with a constant rate 
-        int timesDifferenceInRows = -1;
-        int totalAmmountOfDifferenceInRows = 1;
-        for (int i = 0; i < N; i++)
+        List<int[]> passedCells = new List<int[]>();
+        passedCells.Add(new int[2] { -1, -1 });
+
+        int[] coordinates = { -1, -2 };
+        int sum = 0;
+
+        // MasterList.Any(c => c.SequenceEqual(childList))
+
+        if (!(passedCells.Any(c => c.SequenceEqual(coordinates))))
         {
-            if (i % 2 == 1)             //as said at rows%2 = 0 the difference grows, but as the array starts from 0 in code it should be rows%2 = 1
-            {
-                timesDifferenceInRows += 1; //the difference grows by its base by one at every row%2=0 and begins at -1, So at 0 is 0, at 2 is 1, at 4 is 2 and so on.
-            }
-            totalAmmountOfDifferenceInRows += timesDifferenceInRows;
+            passedCells.Add((int[])coordinates.Clone());
+
+            sum++;
         }
-        ulong endResult = (Convert.ToUInt64(N) - 1) * baseResult + (Convert.ToUInt64(differenceInRows) * Convert.ToUInt64(totalAmmountOfDifferenceInRows)) + 1;
-        Console.WriteLine(endResult);
-        //Console.WriteLine(timesDifferenceInRows);
-        //Console.WriteLine(totalAmmountOfDifferenceInRows);
-        // Console.WriteLine(arr[N - 1,M - 1]);
+
+
+
+        Console.WriteLine(sum);
     }
 
-
-
-    static short[,] ArrayFiller(int N, int M)
-    {
-        short[,] arr = new short[N, M];
-        short arrayFiller = 1;
-        short temp = 1;
-        for (int i = 0; i < N; i++)
-        {
-            arrayFiller = temp;
-            for (int j = 0; j < M; j++)
-            {
-                arr[i, j] = arrayFiller;
-                arrayFiller += 3;
-            }
-            temp += 3;
-        }
-        return arr;
-    }
-
-
-
-    static ulong ArrayCalculator(short[,] arr, int N, int M)
-    {
-        ulong result = 1;
-        bool notcorner = true;
-        int currentRow = 1;
-        int currentCol = 1;
-        int prevRow = 0;
-        int prevCol = 0;
-        while (notcorner)
-        {
-            if (currentCol == 0 && currentRow == N - 1)
-            {
-                notcorner = false;
-            }
-            if (currentRow == N - 1 && currentCol == M - 1)
-            {
-                notcorner = false;
-            }
-            if (prevRow < currentRow && prevCol < currentCol && currentCol < M - 1)
-            {
-                result += Convert.ToUInt64(arr[currentRow, currentCol]);         //Going North-East
-                prevRow += 1;                                   //
-                prevCol += 1;
-                currentRow -= 1;
-                currentCol += 1;
-                continue;
-            }
-            if (prevRow > currentRow && prevCol < currentCol && currentCol < M - 1)
-            {
-                result += Convert.ToUInt64(arr[currentRow, currentCol]);
-                prevRow -= 1;
-                prevCol += 1;                                   //Going South-East
-                currentRow += 1;                                //
-                currentCol += 1;
-                continue;
-            }
-            if (currentCol == M - 1)
-            {
-                result += Convert.ToUInt64(arr[currentRow, currentCol]);
-                prevRow += 1;                                   //Going South-West after hitting a wall.
-                prevCol += 1;
-                currentRow += 1;
-                currentCol -= 1;
-                continue;
-            }
-            if (currentCol == 0)
-            {
-                result += Convert.ToUInt64(arr[currentRow, currentCol]);
-                prevRow += 1;                                   //Going South-East after hitting a wall
-                prevCol -= 1;
-                currentRow += 1;
-                currentCol += 1;
-                continue;
-            }
-            if (prevRow < currentRow && prevCol > currentCol && currentCol >= 0)
-            {
-                result += Convert.ToUInt64(arr[currentRow, currentCol]);           //Going North-West
-                prevRow += 1;
-                prevCol -= 1;
-                currentRow -= 1;
-                currentCol -= 1;
-                continue;
-            }
-            if (prevRow > currentRow && prevCol > currentCol)
-            {
-                result += Convert.ToUInt64(arr[currentRow, currentCol]);
-                prevRow -= 1;
-                prevCol -= 1;                                   //Going South-West
-                currentRow += 1;
-                currentCol -= 1;
-                continue;
-            }
-        }
-        return result;
-    }
 }
-}
+
+/*
+You should be able to use Linq here:
+
+var matchingvalues = myList
+    .Where(stringToCheck => stringToCheck.Contains(myString));
+If you simply wish to return the first matching item:
+
+var match = myList
+    .FirstOrDefault(stringToCheck => stringToCheck.Contains(myString));
+
+if(match != null)
+*/
+
+
+
+
