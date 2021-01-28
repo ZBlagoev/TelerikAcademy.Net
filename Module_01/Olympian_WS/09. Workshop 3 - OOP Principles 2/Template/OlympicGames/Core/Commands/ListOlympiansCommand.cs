bf0be6dae8c54ba2b1aa2d65e8f33aad25dd68fc
ZBlagoev <zhelyazkoblagoev@gmail.com>
2021-01-28 16:49:04 +0200

    Implement ListOlympiansCommand Class;

--- Module_01/Olympian_WS/09. Workshop 3 - OOP Principles 2/Template/OlympicGames/Core/Commands/ListOlympiansCommand.cs
+++ Module_01/Olympian_WS/09. Workshop 3 - OOP Principles 2/Template/OlympicGames/Core/Commands/ListOlympiansCommand.cs
@@ -1,6 +1,10 @@
 ﻿using System.Collections.Generic;
 using OlympicGames.Core.Commands.Abstracts;
 using OlympicGames.Core.Contracts;
+using System.Linq;
+using OlympicGames.Olympics.Contracts;
+using System;
+using System.Text;
 
 namespace OlympicGames.Core.Commands
 {
@@ -13,8 +17,73 @@ namespace OlympicGames.Core.Commands
 
         public override string Execute()
         {
-            // TODO: Implement this
-            throw new System.NotImplementedException();
+            string key = "firstname";
+            string sort = "asc";
+            string keySort;
+
+            switch (this.CommandParameters.Count)
+            {
+                case 1:
+                    key = this.CommandParameters[0];
+                    break;
+                case 2:
+                    key = this.CommandParameters[0];
+                    switch (this.CommandParameters[1])
+                    {
+                        case "desc":
+                            sort = this.CommandParameters[1];
+                            break;
+                        default:
+                            throw new ArgumentException("Invalid parameters!");
+                    }
+                    break;
+                default:
+                    break;
+            }
+
+
+            keySort = key + "-" + sort; 
+
+
+            List<IOlympian> committee = new List<IOlympian>(this.Committee.Olympians);
+
+            if (committee.Count == 0)
+            {
+                return "NO OLYMPIANS ADDED";
+            }
+
+            switch (keySort)
+            {
+                case "firstname-desc":
+                    committee = (List<IOlympian>)committee.OrderByDescending(oly => oly.FirstName).ToList();
+                    break;
+                case "lastname-asc":
+                    committee = (List<IOlympian>)committee.OrderBy(oly => oly.LastName).ToList();
+                    break;
+                case "lastname-desc":
+                    committee = (List<IOlympian>)committee.OrderByDescending(oly => oly.LastName).ToList();
+                    break;
+                case "country-asc":
+                    committee = (List<IOlympian>)committee.OrderBy(oly => oly.Country).ToList();
+                    break;
+                case "country-desc":
+                    committee = (List<IOlympian>)committee.OrderByDescending(oly => oly.Country).ToList();
+                    break;
+                default:
+                    committee = (List<IOlympian>)committee.OrderBy(oly => oly.FirstName).ToList();
+                    break;
+            }
+
+
+            var result = new StringBuilder();
+            result.AppendLine($"Sorted by [key: {key}] in [order: {sort}]");
+
+            foreach (var obj in committee)
+            {
+                result.AppendLine(obj.Print());
+            }
+
+            return result.ToString().Trim();
         }
     }
 }
