8f83d4dcb4e83154755cc2e2dd2b5fb073cf6397
ZBlagoev <zhelyazkoblagoev@gmail.com>
2021-01-28 12:29:16 +0200

    Implement CreateBoxerCommand Class;

--- Module_01/Olympian_WS/09. Workshop 3 - OOP Principles 2/Template/OlympicGames/Core/Commands/CreateBoxerCommand.cs
+++ Module_01/Olympian_WS/09. Workshop 3 - OOP Principles 2/Template/OlympicGames/Core/Commands/CreateBoxerCommand.cs
@@ -2,20 +2,62 @@
 using System.Collections.Generic;
 using OlympicGames.Core.Commands.Abstracts;
 using OlympicGames.Core.Contracts;
+using OlympicGames.Olympics.Contracts;
 
 namespace OlympicGames.Core.Commands
 {
     public class CreateBoxerCommand : Command
     {
-        public CreateBoxerCommand(IList<string> commandLine, IOlympicCommittee committee) 
+        public CreateBoxerCommand(IList<string> commandLine, IOlympicCommittee committee)
             : base(commandLine, committee)
         {
         }
 
         public override string Execute()
         {
-            // TODO: Implement this
-            throw new NotImplementedException();
+            if (this.CommandParameters.Count != 6)
+            {
+                throw new ArgumentOutOfRangeException("ERROR: Parameters count is not valid!");
+            }
+
+            string firstName = this.CommandParameters[0];
+            string lastName = this.CommandParameters[1];
+            string country = this.CommandParameters[2];
+            string category = CommandParameters[3];
+
+
+            int wins;
+
+            bool isIntWins = int.TryParse(this.CommandParameters[4], out wins);
+
+            if (!isIntWins)
+            {
+                throw new ArgumentException("ERRROR: Wins must be an intager number!");
+            }
+
+            int losses;
+
+            bool isIntLosses = int.TryParse(this.CommandParameters[5], out losses);
+
+            if (!isIntLosses)
+            {
+                throw new ArgumentException("ERRROR: Losses must be an intager number!");
+            }
+
+
+            IOlympian olympian = this.Factory.CreateBoxer(firstName, lastName, country, category, wins, losses);
+
+            // check if already exists
+            this.Committee.Add(olympian);
+
+            return "Created boxer";
+            /*
+            Product product = this.productFactory.CreateProduct(name, brand, price, gender);
+
+			this.productRepository.Products.Add(name, product);
+
+			
+            */
         }
     }
 }
